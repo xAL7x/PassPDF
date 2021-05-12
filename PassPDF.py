c608f800fa3c47dad76dee871261c6fcee219edb
BuildTools <cpkhaled@yahoo.com>
2021-05-12 22:43:37 +0300

    Updated PassPDF.py, Made the code cleaner and easier to understand.

--- PassPDF.py
+++ PassPDF.py
@@ -1,13 +1,27 @@
-from PyPDF2 import PdfFileWriter,PdfFileReader
-pdfw=PdfFileWriter()
-print('\033[3;34m')
-pdf=PdfFileReader(input("enter the name file \n "))
-for page_num in range(pdf.numPages):
-    pdfw.addPage(pdf.getPage(page_num))
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import time
 
-password=input("\033[3;31m enter the password >> ")
-pdfw.encrypt(password)
-a=input("\033[3;36m enter the name new file \n")
-with open(a+".pdf","wb")as f:
-    pdfw.write(f)
-    f.close()
+
+def PassPDF():
+    pdf_fw = PdfFileWriter()
+    pdf_file = PdfFileReader(input("What is the file name? "))
+
+    for page_num in range(pdf_file.numPages):
+        pdf_fw.addPage(pdf_file.getPage(page_num))
+
+    file_password = input("Please enter the password you want: ")
+    pdf_fw.encrypt(file_password)
+
+    new_file_name = input("Please enter the new file name: ")
+    with open(f'{new_file_name}.pdf', "wb") as file:
+        pdf_fw.write(file)
+        file.close()
+        print('Done, Successfully added a encrypted the file!')
+
+
+try:
+    PassPDF()
+except KeyboardInterrupt:
+    print('\nExiting!')
+    time.sleep(0.5)
+    quit()
